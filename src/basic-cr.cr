92234b423574d3211aa829787b1e43c70065f1da
D. Shwagginz <93965353+D-Shwagginz@users.noreply.github.com>
2024-04-18 03:46:37 -0700

    working c64 print

---
+++ src/basic-cr.cr
@@ -0,0 +1,30 @@
+require "./basic-cr/**"
+
+module Basic
+  VERSION = 0.1
+
+  enum Type
+    C64
+  end
+
+  struct Line
+    property number, command
+
+    def initialize(number : Int32 = 0, command : String = "NONE")
+      @number = number
+      @command = command
+    end
+  end
+
+  def self.run(type : Type, code : String)
+    case type
+    when Type::C64
+      C64.run(code)
+    end
+  end
+end
+
+Basic.run(Basic::Type::C64,
+  %|
+1 print"Testing"
+|)
